c5f6309192d32f15b91b5cd8167d15698fcb764b
worker <worker@adm-group.ru>
2021-01-27 16:17:51 +0300

    fix big bug from html

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,11 @@ $(function(){
         nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
         infinite: false,
     });
+
+    $('.questions__item-title').on('click', function(){
+
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+    });
     
 });
